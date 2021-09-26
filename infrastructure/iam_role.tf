82675c747a7a94693b4dcf9aafafe3e9448a6ca2
yossy <t.yoshimura0806@gmail.com>
2021-09-26 18:32:35 +0900

    remove example module

--- infrastructure/iam_role.tf
+++
@@ -1,6 +0,0 @@
-module "describe_regions_for_ec2" {
-  source     = "./iam_role"
-  name       = "describe-regions-for-ec2"
-  identifier = "ec2.amazonaws.com"
-  policy     = data.aws_iam_policy_document.allow_describe_regions.json
-}
